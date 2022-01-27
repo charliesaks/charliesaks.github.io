fc8a1688bdb5f00211bf1cdbdfb8e2273bd196a5
Charlie Sakamaki <csakamaki@hawaii.rr.com>
2022-01-26 23:13:16 -1000

    Add OnePirate template, my icons and images

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import * as React from 'react';
-import ReactDOM from 'react-dom';
-import Button from '@mui/material/Button';
+// import Button from '@mui/material/Button';
 import { CssBaseline } from '@mui/material';
+import Home from './Home.js'
 
 function App() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <Button variant="contained">Hello World</Button>
+      <Home />
+      {/* <Button variant="contained">Hello World</Button> */}
     </React.Fragment>
   );
 }
